f1125559c358e5b828af4c9a61604e08915ebe1f
Jan K. Kapala <jankapala@gmail.com>
2019-01-12 11:15:47 +0100

    Add implementation of the kronekerization method in the Step class

--- Step.py
+++ Step.py
@@ -1,47 +1,83 @@
+from Register import Register
+from Gates.Gates import I, C
+
+
 class Step:
     def __init__(self, n):
         self.gates = []
         for i in range(n):
             self.gates.append(I())
-        self.gates = np.array(self.gates)
+
+        self.possibilities = []
         
     def size(self):
         return len(self.gates)
     
     def addGate(self, gate):
-        if issubclass(g.__class__,C):
+        if isinstance(gate,C):
             cq = gate.control_qubit
             tq = gate.target_qubit
             self.gates[cq] = gate
             self.gates[tq] = gate
         else:
             self.gates[gate.qubit_index] = gate
-        
-    def kroneckerize(self):
-        return kronekerize(0, 1)
     
-    def kronekerize(i, last):
-        g = self.gates[i]
-        
-        # TODO END CONDITION
-#         if i == self.size()-1:
-#             return last
-        
-        if issubclass(g.__class__,C):
-            if i == g.control_qubit:
+    def kronekerize(self):
+        possibilities = gen_possibilities()
+        result = 0
+        for possibility in possibilities:
+
+            last = 1
+            for i in range(self.size()):
+                last = np.kron(last, possibility[i].state)
+            result += last
+        return result
+
+    def gen_possibilities(self):
+        raw_gates = list(self.gates)
+
+        # Set all non-control-gates
+        for i in range(self.size()):
+            if not isinstance(raw_gates[i],C):
+                raw_gates[i] = raw_gates[i].matrix
+
+        # Parallely set control-gates
+        return generate_possibilities(0, [raw_gates])
+
+    def gen_possibilities(self, i, possibilities):
+        # RECURSION END CONDITION
+        if i >= self.size():
+            return possibilities
+
+        # Check if the gate of index i has been set before
+        # (checking for one possibility is sufficient)
+        # If has, skip it 
+        if isinstance(possibilities[0][i], numpy.ndarray):
+                return generate_possibilities(i+1, possibilities)
+
+        # Because all non-control-gates have been set before,
+        # only control-gates have to be set.
+        new_possibilities = []
+        for possibility in possibilities:
+            gate = possibility[i]
+            if i == gate.control_qubit:
+                # Do not change control qubit
+                possibility[gate.control_qubit] = I().matrix
+
                 # control qubit == 0 case:
-                self.gates[g.target_qubit]
-                a = kronekerize(i+1, np.kron(np.array([[1,0],[0,0]]), last))
+                # Do not change target qubit
+                0_case = list(possibility)
+                0_case[gate.target_qubit] = I().matrix
                 
                 # control qubit == 1 case:
-                b = kronekerize(i+1, np.kron(np.array([[0,0],[0,1]]), last))
-                return a + b
-            else:
+                # Change target qubit
+                1_case = list(possibility)
+                1_case[gate.target_qubit] = gate.matrix
+
+                # Parallely consider both cases
+                new_possibilities.append(0_case)
+                new_possibilities.append(1_case)
+        return gen_possibilities(i+1, new_possibilities)
                 
-            
-        else:
-            return kronekerize(i+1, np.kron(g.matrix, last))
-        
-    
     def __repr__(self):
         return str(s.gates)
